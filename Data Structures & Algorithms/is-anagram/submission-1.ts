ab830041701a3747be3c6930f84fb8f91295ef56
Shivank Jain <56064247+ShivankJain@users.noreply.github.com>
2026-06-15 07:51:00 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+      if(s.length != t.length) return false;
+
+      const ss: string[] = [...s].sort();
+      const tt: string[] = [...t].sort();  
+
+      let n: number = 0;
+      while(n < s.length){
+        if(ss[n] != tt[n]) return false;
+        n++;
+      }
+
+      return true;
+    }
+}
